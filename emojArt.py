7b2674ef5697ef186f2f5813f9334648cd6c3c77
kitvss <vladimir.s.tkachuk@gmail.com>
2023-02-12 17:37:30 +0200

    Add files via upload

---
+++ emojArt.py
@@ -0,0 +1,10 @@
+#while loop
+lines = 1
+emoji = "*"
+while lines <= 9:
+    print(lines * emoji)
+    lines += 1
+
+#for loop
+for num in range(1, 10):
+    print(emoji * num)
---
+++ stopCopiyng.py
@@ -0,0 +1,5 @@
+text = input("Hey, how it's going? \n").lower()
+while text != "stop copying me":
+    print(text)
+    text = input()
+print("UGH FINE YOU WIN")
